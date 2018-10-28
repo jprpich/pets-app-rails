78cb66e6e3033c1af8b862411ff81805ff3aef13
Joshua <joshuaprpich@gmail.com>
2018-10-28 18:40:54 -0500

    use form_tags instead of form_for helpers for practice

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -12,6 +12,20 @@ class PetsController < ApplicationController
     redirect_to '/pets'
   end
 
+  def show
+    @pet = Pet.find(params[:id])
+  end
+
+  def edit
+    @pet = Pet.find(params[:id])
+  end
+
+  def update
+    @pet = Pet.find(params[:id])
+    @pet.update(pet_params)
+    redirect_to '/pets'
+  end
+
   private
     def pet_params
       params.require(:pet).permit(:name)
