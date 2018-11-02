86c84e6fa43c53bb2406febe5f84e07996ded70a
Joshua <joshuaprpich@gmail.com>
2018-11-02 09:18:23 -0500

    add resources :dogs and use helper methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/pets', to: 'pets#index'
-  get '/pets/new', to: 'pets#new'
-  post '/pets', to: 'pets#create'
-  get '/pets/:id', to: 'pets#show'
-  get '/pets/:id/edit', to: 'pets#edit'
-  patch '/pets/:id', to: 'pets#update'
-  delete '/pets/:id/delete', to: 'pets#destroy'
+  # get '/pets', to: 'pets#index', as: "pets"
+  # get '/pets/new', to: 'pets#new', as: "new_pet"
+  # post '/pets', to: 'pets#create'
+  # get '/pets/:id', to: 'pets#show', as: "pet"
+  # get '/pets/:id/edit', to: 'pets#edit', as: "edit_pet"
+  # patch '/pets/:id', to: 'pets#update'
+  # delete '/pets/:id', to: 'pets#destroy'
+
+  resources :pets
 end
